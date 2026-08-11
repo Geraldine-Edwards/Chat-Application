0823e3456bfb91c4ed1fb1fa1b9eb49913656f37
Geraldine-Edwards <geraldine.edwards25@gmail.com>
2026-08-12 00:03:07 +0100

    fix: Update backend URL for new deployment

--- websockets-frontend/config.js
+++ websockets-frontend/config.js
@@ -2,5 +2,5 @@ export const API_BASE_URL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000"
     : window.location.hostname === "127.0.0.1"
-    ? "http://127.0.0.1:3000"
-    : "https://geraldine-edwards-chat-app-websockets-backend.hosting.codeyourfuture.io";
+      ? "http://127.0.0.1:3000"
+      : "https://ge-chat-app-websockets-backend.trainees.hosting.cyf.academy";
